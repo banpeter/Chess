3e748060013719fea1bb2f341bf7d4ac49b22f82
banpeter <bn.peter.hun@gmail.com>
2022-09-16 21:15:56 +0200

    choosing the next move

--- chess.c
+++ chess.c
@@ -35,6 +35,7 @@ int main()
         {-1,-2},// ^<<
         {-2,-1},//^^<
     };
+    //next posible positions from x,y coordinates
     int temp_cor[8][2]={
         {0,0},
         {0,0},
@@ -50,26 +51,50 @@ int main()
     //temporary coordinates so we can check the future steps
     int temp_x,temp_y;
     int c=1;
+    //largest moves
+    int max,count,nom;//nom=number of move = index of move
     //we check all the next move possible
     //after that we evaluate how many steps we can take after that
     //we choose the biggest value
 
     while(c!=0){
-        for(int i=0;i<8;i++){
+        // for every coodinates we go through all the possible moves
+        for(int i=0;i<=8;i++){
+
             temp_x = x+moves[i][0];
-            temp_y = moves[i][1]+y;
+            temp_y = y+moves[i][1];
             if(validate(temp_x,temp_y)){
                     //temp
                     temp_cor[i][0] = temp_x;
-                    temp_cor[i][0] = temp_y;
+                    temp_cor[i][1] = temp_y;
             }
             else{
                 temp_cor[i][0] = -1;
-                temp_cor[i][0] = -1;
+                temp_cor[i][1] = -1;
             }
         }
-        //evaluate temp_moves and choose the largest
 
+        //evaluate temp_moves and choose the largest
+        //go through the temp_moves with the same algorithm
+        max=0;
+        for(int j=0;j<=8;j++){//check all the temporary coordinates
+            count=0;
+            for(int k=0;k<=8;k++){//moves
+                temp_x = temp_cor[j][0]+moves[k][0];
+                temp_y = temp_cor[j][1]+moves[k][1];
+                if(validate(temp_x,temp_y)){
+                        count++;
+                }
+            }
+            if(count>max){
+                max=count;
+                nom=j;
+            }
+            
+        }
+        //taking the next step
+        x=temp_cor[nom][0];
+        y=temp_cor[nom][1];
         c++;
 
     }
